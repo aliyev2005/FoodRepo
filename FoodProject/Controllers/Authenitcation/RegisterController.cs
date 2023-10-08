2febf1ddf3acd7b49b2e34ed99f950a0f5f86417
Elnur <entreur@gmail.com>
2023-10-08 13:56:59 +0400

    Register, Login, Logout in progress

---
+++ FoodProject/Controllers/Authenitcation/RegisterController.cs
@@ -0,0 +1,44 @@
+﻿using CryptoHelper;
+using FoodProject.Data;
+using FoodProject.DTO;
+using FoodProject.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodProject.Controllers.Authenitcation
+{
+    public class RegisterController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public RegisterController(ApplicationDbContext context)
+        {
+            _context = context;   
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        [HttpPost]
+        public IActionResult Index(RegisterRequest request)
+        {
+            Console.WriteLine("Create method working!");
+            if (_context.Users.Any(u => u.Email == request.Email))
+            {
+                ModelState.AddModelError("Email", "The email is already in use");
+            }
+            if (ModelState.IsValid)
+            {
+                User user = new()
+                {
+                    Email = request.Email,
+                    Fullname = request.Fullname,
+                    Password = Crypto.HashPassword(request.Password),  
+                };
+                _context.Users.Add(user);
+                _context.SaveChanges();
+                return RedirectToAction("index", "admin");
+            }
+            return View(request);
+        }
+    }
+}
