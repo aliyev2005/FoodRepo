64430dde6468417290652eac2d7a139bf66d672c
Elnur <entreur@gmail.com>
2023-10-09 11:29:21 +0400

    Food model / controller

---
+++ FoodProject/Controllers/FoodController.cs
@@ -0,0 +1,21 @@
+﻿using FoodProject.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodProject.Controllers
+{
+    public class FoodController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public FoodController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        [Route("/api/food")]
+        public IActionResult Index()
+        {
+            var food = _context.Foods.ToList();
+            return Ok(food);
+        }
+    }
+}
---
+++ FoodProject/Model/Food.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodProject.Model
+{
+    public class Food:BaseModel
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
