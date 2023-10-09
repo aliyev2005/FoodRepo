6014367679004f8c6e228053524cf227d6ba742f
Elnur <entreur@gmail.com>
2023-10-09 12:09:03 +0400

    API Key insert

--- FoodProject/Program.cs
+++ FoodProject/Program.cs
@@ -1,5 +1,7 @@
+using FoodProject.Controllers.Authorization.Filter;
 using FoodProject.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +13,35 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api Key Auth", Version = "v1" });
+    c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+    {
+        Description = "ApiKey must appear in header",
+        Type = SecuritySchemeType.ApiKey,
+        Name = "x-api-key",
+        In = ParameterLocation.Header,
+        Scheme = "ApiKeyScheme"
+    });
+    var key = new OpenApiSecurityScheme()
+    {
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "ApiKey"
+        },
+        In = ParameterLocation.Header
+    };
+    var requirement = new OpenApiSecurityRequirement
+                    {
+                             { key, new List<string>() }
+                    };
+    c.AddSecurityRequirement(requirement);
+});
+
+builder.Services.AddScoped<ApiKeyAuthFilter>();
+builder.Services.AddScoped<UserAuthenticationAuth>();
 
 WebApplication? app = builder.Build();
 
