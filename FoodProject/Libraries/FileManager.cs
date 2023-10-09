bf53d4095f0b8a91fa702fc014c330e3376314ca
Elnur <entreur@gmail.com>
2023-10-09 12:29:54 +0400

    File upload mechanism

---
+++ FoodProject/Libraries/FileManager.cs
@@ -0,0 +1,25 @@
+﻿using FoodProject.Libraries.Repository;
+
+namespace FoodProject.Libraries
+{
+    public class FileManager : IFileManager
+    {
+        //private const string _PATH = "Uploads";
+        private const string _ROOT = "Uploads";
+
+        void IFileManager.Delete(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IFileManager.FileExists(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        string IFileManager.Upload(IFormFile file, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FoodProject/Libraries/Repository/IFileManager.cs
@@ -0,0 +1,9 @@
+﻿namespace FoodProject.Libraries.Repository
+{
+    public interface IFileManager
+    {
+        string Upload(IFormFile file, string fileName = "");
+        void Delete(string fileName);
+        bool FileExists(string fileName);
+    }
+}
