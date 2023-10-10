5624b1eb04d0e45f817df1d6308f1cb704e4e981
Elnur <entreur@gmail.com>
2023-10-10 13:20:48 +0400

    City enums

---
+++ FoodProject/Enums/Cities.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Serialization;
+
+namespace FoodProject.Enums
+{
+    public enum Cities
+    {
+        [EnumMember(Value ="Baku")]
+        BAKU,
+        [EnumMember(Value = "Ganja")]
+        GANJA,
+        [EnumMember(Value = "Quba")]
+        QUBA,
+        [EnumMember(Value = "Shaki")]
+        SHAKI,
+        [EnumMember(Value = "Shamaxi")]
+        SHAMAXI,
+        [EnumMember(Value = "Qabala")]
+        QABALA,
+        /// <summary>
+        /// Lankaran
+        /// </summary>
+        //[EnumMember(Value = "Lankaran")]
+        LANKARAN
+    }
+}
